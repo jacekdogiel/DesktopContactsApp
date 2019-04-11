00f4d479560097664d6fb8b7f477a29c0f08a039
jacek.dogiel@gmail.com <jacek.dogiel@gmail.com>
2019-04-11 19:41:16 +0200

    Added listView

--- DesktopContactsApp/Classes/Contact.cs
+++ DesktopContactsApp/Classes/Contact.cs
@@ -1,9 +1,4 @@
 ﻿using SQLite;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DesktopContactsApp.Classes
 {
@@ -14,5 +9,10 @@ namespace DesktopContactsApp.Classes
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Email} - {Phone}";
+        }
     }
 }
--- DesktopContactsApp/MainWindow.xaml.cs
+++ DesktopContactsApp/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using DesktopContactsApp.Classes;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DesktopContactsApp
 {
@@ -38,10 +27,15 @@ namespace DesktopContactsApp
 
         void ReadDatabase()
         {
+            List<Contact> contacts;
             using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.databasePath))
             {
                 conn.CreateTable<Contact>();
-                var contacts = conn.Table<Contact>().ToList();
+                contacts = conn.Table<Contact>().ToList();
+            }
+            if (contacts != null)
+            {
+                contactsListView.ItemsSource = contacts;
             }
         }
     }
