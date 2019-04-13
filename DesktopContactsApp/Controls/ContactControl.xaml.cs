3463a14793149a39b1a2d59276c5765b3f64dfd1
jacek.dogiel@gmail.com <jacek.dogiel@gmail.com>
2019-04-13 12:54:53 +0200

    Added custom control

---
+++ DesktopContactsApp/Controls/ContactControl.xaml.cs
@@ -0,0 +1,42 @@
+﻿using DesktopContactsApp.Classes;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace DesktopContactsApp.Controls
+{
+    /// <summary>
+    /// Interaction logic for ContactControl.xaml
+    /// </summary>
+    public partial class ContactControl : UserControl
+    {
+
+
+        public Contact Contact
+        {
+            get { return (Contact)GetValue(ContactProperty); }
+            set { SetValue(ContactProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Contact.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ContactProperty =
+            DependencyProperty.Register("Contact", typeof(Contact), typeof(ContactControl), new PropertyMetadata(new Contact() { Name = "Name", Phone = "123456", Email = "abc@abc.com" }, SetText));
+
+        private static void SetText(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as ContactControl;
+
+            if (control != null)
+            {
+                control.nameTextBlock.Text = (e.NewValue as Contact).Name;
+                control.emailTextBlock.Text = (e.NewValue as Contact).Email;
+                control.phoneTextBlock.Text = (e.NewValue as Contact).Phone;
+            }
+        }
+
+        public ContactControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
